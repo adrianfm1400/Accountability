1cd1ffef5cd2088a85833529fb1c00431bc18d8f
adrianfm1400 <79126409+adrianfm1400@users.noreply.github.com>
2023-03-08 15:40:32 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 function ejecutarPython() {
     var spawn = require("child_process").spawn;
-    var proceso = spawn('python', ['./prueba.py']);
+    var proceso = spawn('python', ['./python_rev_v1.py']);
     proceso.stdout.on('data', function (data) {
         console.log(data.toString());
     });
